40c5483180b3c983931ac3f7eb4877fbfcbab35d
Elias Carcamo <el.carcamo@duocuc.cl>
2026-03-30 11:35:39 -0300

    Repository creado

---
+++ src/main/java/cl/duoc/formativa/repository/ProductRepository.java
@@ -0,0 +1,37 @@
+package cl.duoc.formativa.repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.ArrayList;
+import org.springframework.stereotype.Repository;
+
+import cl.duoc.formativa.model.Product;
+
+@Repository
+public class ProductRepository {
+    private final Map<Long, Product> productStore = new HashMap<>();
+    public  List<Product> findAll(){
+        return new ArrayList<>(productStore.values());
+
+    }
+
+    public Optional<Product> findById(Long id){
+        return Optional.ofNullable(productStore.get(id));
+    }
+    public Product save(Product product){
+        productStore.put(product.getId(), product);
+        return product;
+    }
+
+    public void deleteById(Long id){
+        productStore.remove(id);
+    }
+
+    public boolean existById(Long id){
+        return productStore.containsKey(id);
+    }
+
+
+}
